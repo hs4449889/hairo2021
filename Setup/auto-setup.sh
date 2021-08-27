6a59150ced1641d4fde67f473025e4dd4951887a
hs4449889 <84255897+hs4449889@users.noreply.github.com>
2021-08-27 14:30:03 +0900

    Update auto-setup.sh

--- Setup/auto-setup.sh
+++ Setup/auto-setup.sh
@@ -1,13 +1,13 @@
 echo "==========start setup ssh,ip fixed,wifi =========="
 
 #wifiを設定
-cp wpa_supplicant.conf /etc/wpa_supplicant.conf
+sudo cp wpa_supplicant.conf /etc/wpa_supplicant.conf
 
 #ssh有効化
-mkdir /boot/ssh
+sudo mkdir /boot/ssh
 
 #ip固定化
-cp dhcpcd.conf /etc/dhcpcd.conf
+sudo cp dhcpcd.conf /etc/dhcpcd.conf
 
 #再起動
 sudo reboot
