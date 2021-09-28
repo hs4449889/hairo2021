54bcd6c813be63d538ff60a5372fdd815203c70b
HS-1114G <77729732+HS-1114G@users.noreply.github.com>
2021-09-28 18:24:35 +0900

    [Fix] 座標取得用関数
    ・get_corner
    ・リアルセンスで高解像度のフレーム取得開始
    ・クリックすることで2点分の座標を得る
    ・高解像度フレーム取得を終了する
    
    ※高解像度フレームを取得するために、メインでのリアルセンス使用の停止が求められる可能性あり。しかし、ポイント取得時にのみ高画質データを取得することで、ラズパイへの負荷を軽減できると考えられる。

--- Arm/Arm_main.py
+++ Arm/Arm_main.py
@@ -4,6 +4,27 @@ import realsense
 import struct
 from timeout_decorator import timeout, TimeoutError
 
+# unit
+#   angle  = degree
+#   lenght = mm
 
-# settings
-arm = arm_inverse.ARMS([1, 1, 1], [10, 160, -80])
+# auto_moving_arm_class
+class AutoArm:
+    def __init__(self):
+        # settings
+        self.arm        = arm_inverse.ARMS([400, 400, 100], [10, 160, -80])
+        self.corner_xyz = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+    
+    def get_corner(self):
+        self.realsense  = realsense.REALSENSE([1280, 720], 15)
+        _count = 0
+        while _count<2:
+            # take_pictures
+            self.realsense.Get_Frame()
+            # if_clicked, return_positions
+            if realsense.depth != 0:
+                self.corner_xyz[_count][0] = realsense.world_point[0] * 1000
+                self.corner_xyz[_count][1] = realsense.world_point[1] * 1000
+                self.corner_xyz[_count][2] = realsense.depth * 1000
+                count += 1
+        self.realsense.pipeline.stop()
