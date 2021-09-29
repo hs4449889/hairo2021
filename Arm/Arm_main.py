c8a02177cacef03b5524ae56bc356efe4ec8e17f
HS-1114G <77729732+HS-1114G@users.noreply.github.com>
2021-09-29 15:01:51 +0900

    [add] tcp_files
    ・send = コントローラ/センサを指す方
    　コントローラの値を送る
    
    ・read = モータを指す方/サブの方
    　コントローラの値を受け取る

---
+++ Arm/Arm_main.py
@@ -0,0 +1,72 @@
+# import
+import arm_inverse
+import realsense
+import struct
+import math
+import numpy as np
+from timeout_decorator import timeout, TimeoutError
+
+# unit
+#   angle  = degree
+#   lenght = mm
+
+# auto_moving_arm_class
+class AutoArm:
+    def __init__(self):
+        # settings
+        self.arm        = arm_inverse.ARMS([400, 400, 100], [10, 160, -80])
+        self.corner_xyz = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+
+        # must_adjust
+        self.PEN_WIDTH  = 20
+        self.MAX_PULTH  = 2500
+        self.MIN_PULTH  = 500
+
+    def deg_to_pulth(self, x):
+        _pulths = (x+180)*(MAX-MIN)/360
+        return _pulths
+
+    def get_corner(self):
+        self.realsense  = realsense.REALSENSE([1280, 720], 15)
+        _count = 0
+        while _count<2:
+            # take_pictures
+            self.realsense.Get_Frame()
+            # if_clicked, return_positions
+            if realsense.depth != 0:
+                self.corner_xyz[_count][0] = realsense.world_point[0] * 1000
+                self.corner_xyz[_count][1] = realsense.world_point[1] * 1000
+                self.corner_xyz[_count][2] = realsense.depth * 1000
+                count += 1
+        self.realsense.pipeline.stop()
+
+    def get_draw_route(self):
+        # consider_y_axis
+        area_height = abs(self.corner_xyz[0][1] - self.corner_xyz[1][1])
+        self.steps  = math.floor(area_height / self.PEN_WIDTH)
+
+        # consider_x_axis(first_route)
+        self.arm.Setting([self.corner_xyz[0][0], self.corner_xyz[0][2]])
+        self.arm.Moving()
+        self.angles_transition_first = self.deg_to_pulth(self.arm.angles_transition.T)
+
+        # consider_x_axis(normal_route)
+        self.arm.Setting([self.corner_xyz[1][0], self.corner_xyz[1][2]])
+        self.arm.Moving()
+        self.angles_transition_normal =  self.deg_to_pulth(self.arm.angles_transition.T)
+
+    def select_route_x(self, mode, direction):
+        """
+        mode : fisrt_move = 0 / normal_mode = 1
+        dir  : left = 0       / right = 1
+        """
+
+        # set_route
+        if   mode==0:
+            self.next_route = self.angles_transition_first
+        elif direction==1:
+            self.next_route = self.angles_transition_normal
+        else:
+            self.next_route = np.flipud(self.angles_transition_normal)
+
+    
