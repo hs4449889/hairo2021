3ccea5c4feabda743ef4762aa614a703f6b26053
hs4449889 <shida.haruhi@gmail.com>
2021-10-04 15:53:19 +0900

    [add]Motor classのdocstringsを作成した
    日本語でraspberr pi上で動作するかはわからないが
    とりあえず日本語で書く

--- Motor/dc_motor.py
+++ Motor/dc_motor.py
@@ -3,6 +3,18 @@ import RPi.GPIO as GPIO
 import time 
 
 class DcMotor:
+    """
+    DCモーターを使用するためのclass
+    pwm制御用のpinやdirction用のpin番号(BCM)を保持する
+
+    Parameters
+    ----------
+    pwm_pin : int
+        モータードライバのpwm制御用pin
+    
+    dir_pin : int
+        モータードライバのdirection制御用のpin
+    """
     def __init__(self,pwm_pin = 19,dir_pin = 26):
         
         self.pwm_pin = pwm_pin
@@ -18,8 +30,27 @@ class DcMotor:
         GPIO.output(self.dir_pin,True)
         
     def rotation(self,Direction = True,Rotation_Speed = 100):
+        """
+        DCモーターを回転させるための関数
+
+        Parameters
+        ----------
+        direction : int
+            方向を保持する変数(True = 正回転、False = 負の回転)
+
+        rotation_speed : int
+            ChangeDutyCycleの値を保持する変数(0 ~ 100)
+        """
         GPIO.output(self.dir_pin,Direction)
         self.pwm_pin.ChangeDutyCycle(Rotation_Speed)
         
     def finish(self):
+        """
+        pwm制御を停止するための関数
+
+        Parameters
+        ----------
+        
+        None
+        """
         self.pwm_pin.stop()
