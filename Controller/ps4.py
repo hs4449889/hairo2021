e942cc3f76807ba49902f8d751a452b4f671ec24
hs4449889 <shida.haruhi@gmail.com>
2021-10-05 17:37:07 +0900

    [upd]controller classを更新した
    ・レバーもR2,L2も最高値を取るときのみ検知する設定にしたので実質ボタン
    計23個のボタンが有る
    ・値設定しんどいがな~~~

---
+++ Controller/ps4.py
@@ -0,0 +1,166 @@
+#PS4　class
+
+#TO DO : valueが可変するボタンに対応する
+class Ps4:
+    """
+    Ps4のボタンが押されてるかどうか？を判定する(現状)
+
+    if(ps4.function):
+        #押されてたら実行
+    
+    ボタンの関数に関してはそのボタンが押されていればTrueを返す
+    (もっといい既存のライブラリ無いかな~~)
+    
+    注意：上記の理由により説明を省略する
+    """
+    def __init__(self):
+        pass
+    
+    #図形ボタン4つ
+    def maru_button_pushed(self):
+        
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def batu_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+    def sankaku_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+    def shikaku_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    #矢印ボタン4つ
+    def right_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+    def left_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+    def up_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+    def down_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    #R1,R2
+    def R1_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def L1_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    #その他
+    def share_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def options_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def home_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+            return judg
+
+    def leverL_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    #LeverL
+    def leverL_right_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def leverL_left_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+    
+    def leverL_up_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def leverL_down_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    #LeverR
+    def leverR_right_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def leverR_left_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def leverR_up_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def leverR_down_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+    
+    #R2,L2
+
+    def R2_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
+    def L2_button_pushed(self):
+        #条件
+        if index == 1 and code ==2 :
+            judg = True
+        return judg
+
