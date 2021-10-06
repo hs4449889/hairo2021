30abb9940e6b85c97a4d3f930b79fcf9f04ec537
hs4449889 <shida.haruhi@gmail.com>
2021-10-06 17:49:27 +0900

    [upd]mecanum_stop関数を追加した。
    ・動作指示用の関数の値設定は必要性がないと設定する意欲がわかないのでので省略します。

--- Mecanum/mecanum.py
+++ Mecanum/mecanum.py
@@ -14,16 +14,17 @@ class MecanumManager(object):
 
     """
     #// to do //配列にまとめるかどうか相談
+    #最終的に辞書を採用(配列は可読性が低い)
     def __init__(self,
-            front_left = dc_motor.DcMotor(pwm_pin = 27, dir_pin = 17),
-            front_right = dc_motor.DcMotor(pwm_pin = 14, dir_pin = 15),
-            rear_left = dc_motor.DcMotor(pwm_pin = 26, dir_pin = 19),
-            rear_right = dc_motor.DcMotor(pwm_pin = 20, dir_pin = 21)):
+            front_left  = {"pwm_pin":1,"dir_pin":1},
+            front_right = {"pwm_pin":1,"dir_pin":1},
+            rear_left   = {"pwm_pin":1,"dir_pin":1},
+            rear_right  = {"pwm_pin":1,"dir_pin":1} ):
         
-        self.front_left  = front_left
-        self.front_right = front_right
-        self.rear_left   = rear_left
-        self.rear_right  = rear_right
+        self.front_left  = dc_motor.DcMotor(pwm_pin = front_left["pwm_pin"],dir_pin= front_left["dir_pin"])
+        self.front_right = dc_motor.DcMotor(pwm_pin = front_right["pwm_pin"],dir_pin= front_right["dir_pin"])
+        self.rear_left   = dc_motor.DcMotor(pwm_pin = rear_left["pwm_pin"],dir_pin= rear_left["dir_pin"])
+        self.rear_right  = dc_motor.DcMotor(pwm_pin = rear_right["pwm_pin"],dir_pin= rear_right["dir_pin"])
 
         self.specify_motion = [-1,-1,-1,-1]
 
@@ -34,16 +35,35 @@ class MecanumManager(object):
         if self.specify_motion[2] != -1: self.rear_left.rotation(Direction=self.specify_motion[2], Rotation_Speed=80)
         if self.specify_motion[3] != -1: self.rear_right.rotation(Direction=self.specify_motion[3], Rotation_Speed=80)
 
+    def mecanum_stop(self):
+        if self.specify_motion[0] == -1: self.front_left.rotation(Direction=self.specify_motion[0], Rotation_Speed=0)
+        if self.specify_motion[1] == -1: self.front_right.rotation(Direction=self.specify_motion[1], Rotation_Speed=0)
+        if self.specify_motion[2] == -1: self.rear_left.rotation(Direction=self.specify_motion[2], Rotation_Speed=0)
+        if self.specify_motion[3] == -1: self.rear_right.rotation(Direction=self.specify_motion[3], Rotation_Speed=0)
 
+
+    #値設定は後回しで
     def left_translation(self):
+        self.specify_motion[,,,]
+
     def right_translation(self):
+        self.specify_motion[,,,]
+
     def up_translation(self):
+        self.specify_motion[,,,]
+
     def down_translation(self):
+        self.specify_motion[,,,]
+
 
     def left_turn(self):
+        self.specify_motion[,,,]
+
     def right_turn(self):
+        self.specify_motion[,,,]
 
     def stop(self):
+        self.specify_motion[-1,-1,-1,-1]
 
 
 
